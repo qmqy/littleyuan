4424cb021ced42afe2459e89ef2830b27c8b577b
qmqy <92425530+qmqy@users.noreply.github.com>
2026-09-20 00:22:21 +0800

    0919f

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 如鸢招募 · Service Worker（本地缓存，加载过一次后秒开、断网也能用） */
-const VERSION = 'v0917i';
+const VERSION = 'v0919f';
 const CORE_CACHE = 'yiruan-core-' + VERSION;
 const IMG_CACHE  = 'yiruan-img-'  + VERSION;
 
